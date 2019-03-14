8475de10ce5c355175471662ac40e1a46bdaf0b9
liuchuan <xyz8279635@163.com>
2019-03-15 00:21:28 +0800

    new Spider

--- index.py
+++ index.py
@@ -4,10 +4,41 @@
 # @Author : Liuchuan
 # @File   : index.py
 
-from cookies import index as login
+# from cookies import index as login
+# login_cookies = login.get_logging_cookies()
+# print(login_cookies)
 
-login_cookies = login.get_logging_cookies()
+import requests
+from lxml import etree
 
-print(login_cookies)
+class Spider:
+    @staticmethod
+    def start_request_xiaomuchong(website):
+        response = requests.get(website)
+        print(response.text)
+        print(response)
 
 
+class Muchong(object):
+    def __init__(self, start_website):
+        self.website = start_website
+        self.html = None
+
+    def start_request(self):
+        response = requests.get(self.website)
+        print(response.text)
+        self.html = response.text
+        pass
+
+    def get_atricle_links(self):
+        self.start_request()
+        Selector = etree.HTML(self.html)
+        url_list = Selector.xpath('//td[@class="xmc_lp20"]/a/@href')
+        print(url_list)
+
+
+
+muchong_website = 'http://muchong.com/bbs/kaoyan.php?action=adjust&type=1'
+
+xiaomuchong = Muchong(muchong_website)
+xiaomuchong.get_atricle_links()
